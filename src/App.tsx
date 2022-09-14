eefb966b79538227bf2b33cc5b362fe34b218a42
HamInGyu <hfire1207@gmail.com>
2022-09-15 03:37:26 +0900

    투두리스트 과제 전단계까지 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import GlobalStyle from "./GlobalStyle";
+import ToDoList from "./components/TodoList";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <ToDoList />
     </>
   );
 }
